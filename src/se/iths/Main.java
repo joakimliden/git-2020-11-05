fd6f4aad51301a4751958ffbdc03e49c8201662e
Your Name <joakim.liden@iths.se>
2020-11-05 11:08:15 +0100

    Fourth version, with ne feature!

--- src/se/iths/Main.java
+++ src/se/iths/Main.java
@@ -3,6 +3,6 @@ package se.iths;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Third version, with ne feature!");
+        System.out.println("Fourth version, with ne feature!");
     }
 }
